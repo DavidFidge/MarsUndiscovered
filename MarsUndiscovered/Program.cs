a4f31696702e2dd165427f80e081dc30c608efd6
David Fidge <dfidge@internode.on.net>
2021-11-28 13:11:06 +1030

    Replace old .net 4 projects with .net core 3.1

--- MarsUndiscovered/Program.cs
+++ MarsUndiscovered/Program.cs
@@ -1,32 +1,14 @@
 ﻿using System;
 
-using MarsUndiscovered.Installers;
-
-using Castle.Windsor;
-
-using FrigidRogue.MonoGame.Core.Interfaces.Components;
-
 namespace MarsUndiscovered
 {
-#if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            var container = new WindsorContainer();
-
-            container.Install(new GameInstaller());
-
-            using (var game = container.Resolve<IGame>())
+            using (var game = new Game1())
                 game.Run();
         }
     }
-#endif
 }
