a29a6a688001101a7c45ae8f74d53f2efdc71fa8
David Fidge <dfidge@internode.on.net>
2022-07-21 15:01:58 +1000

    wip

---
+++ MarsUndiscovered/Components/Items/IronSpike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using MonoGame.Extended;
+
+namespace MarsUndiscovered.Components
+{
+    public class IronSpike : Weapon
+    {
+        private Attack _meleeAttack = new Attack(new Range<int>(5, 9));
+
+        public override string Name => nameof(MagnesiumPipe);
+
+        public override void ApplyProperties(Item item)
+        {
+            base.ApplyProperties(item);
+
+            item.MeleeAttack = (Attack)_meleeAttack.Clone();
+            item.MeleeAttack.SetPowerLevel(item.EnchantmentLevel);
+        }
+
+        public override string GetDescription(Item item, ItemDiscovery itemDiscovery, ItemTypeDiscovery itemTypeDiscovery, int quantity)
+        {
+            var baseDescription = base.GetDescription(item, itemDiscovery, itemTypeDiscovery, quantity);
+
+            if (!String.IsNullOrEmpty(baseDescription))
+                return baseDescription;
+
+            if (!itemDiscovery.IsEnchantLevelDiscovered)
+                return $"An Iron Spike";
+
+            return $"A {GetEnchantText(item)} Iron Spike";
+        }
+    }
+}
