58d4ba51cda6950194a7c94e50fce98e6620bcdb
David Fidge <dfidge@internode.on.net>
2023-05-21 15:27:13 +0930

    Progress with adding internal walls to areas.

--- MarsUndiscovered.Game/Components/Maps/GenerationSteps/InternalWallsGeneration.cs
+++ MarsUndiscovered.Game/Components/Maps/GenerationSteps/InternalWallsGeneration.cs
@@ -1,4 +1,5 @@
-﻿using GoRogue.MapGeneration;
+﻿using FrigidRogue.MonoGame.Core.Extensions;
+using GoRogue.MapGeneration;
 using GoRogue.MapGeneration.ContextComponents;
 using GoRogue.Random;
 using MarsUndiscovered.Game.Components.Maps;
@@ -13,6 +14,7 @@ public class InternalWallsGeneration : GenerationStep
     private readonly int _splitFactor;
     public string StepFilterTag { get; set; }
     public string AreasComponentTag { get; set; }
+    public string AreaWallsDoorsComponentTag { get; set; }
 
     public IEnhancedRandom RNG { get; set; } = GlobalRandom.DefaultRNG;
 
@@ -34,23 +36,63 @@ public class InternalWallsGeneration : GenerationStep
             .Where(a => String.IsNullOrEmpty(StepFilterTag) || a.Step == StepFilterTag)
             .ToList();
 
+        var areaWallsDoors = context.GetFirstOrNew(() => new ItemList<AreaWallsDoors>(), AreaWallsDoorsComponentTag);
+
         foreach (var area in areas.Select(a => a.Item))
         {
-            var splitVertical = area.Bounds.Width > area.Bounds.Height;
-            var splitLength = splitVertical ? area.Bounds.Width : area.Bounds.Height;
-            
-            if (splitLength <= _splitFactor)
-                continue;
-            
-            if (splitVertical && area.Bounds.Width <= 5)
-                splitVertical = false;
+            var doors = new List<Point>();
+            var walls = new List<Point>();
 
-            if (splitVertical)
-            {
-                var splitPoint = RNG.NextInt(area.Bounds.MinExtentX + 1, area.Bounds.MaxExtentX - 1);
-                var area1 = 
-                
-            }
+            SplitAreaRecursive(area, walls, doors);
+
+            areaWallsDoors.Add(
+                new AreaWallsDoors
+                {
+                    Area = area,
+                    Walls = walls,
+                    Doors = doors
+                },
+                Name
+                );
         }
+
+        yield return null;
+    }
+
+    private void SplitAreaRecursive(Area area, List<Point> walls, List<Point> doors)
+    {
+        var splitVertical = area.Bounds.Width > area.Bounds.Height;
+        var splitLength = splitVertical ? area.Bounds.Width : area.Bounds.Height;
+
+        if (splitLength <= _splitFactor)
+            return;
+
+        List<Point> newWallPoints;
+        Area splitArea1;
+        Area splitArea2;
+
+        if (splitVertical)
+        {
+            var splitPoint = RNG.NextInt(area.Bounds.MinExtentX + 1, area.Bounds.MaxExtentX - 1);
+            newWallPoints = area.Where(p => p.X == splitPoint).ToList();
+            splitArea1 = new Area(area.Where(p => p.X < splitPoint));
+            splitArea2 = new Area(area.Where(p => p.X > splitPoint));
+        }
+        else
+        {
+            var splitPoint = RNG.NextInt(area.Bounds.MinExtentY + 1, area.Bounds.MaxExtentY - 1);
+            newWallPoints = area.Where(p => p.Y == splitPoint).ToList();
+            splitArea1 = new Area(area.Where(p => p.Y < splitPoint));
+            splitArea2 = new Area(area.Where(p => p.Y > splitPoint));
+        }
+        
+        var door = RNG.RandomIndex(newWallPoints);
+        doors.Add(newWallPoints[door]);
+        newWallPoints.RemoveAt(door);
+        
+        walls.AddRange(newWallPoints);
+        
+        SplitAreaRecursive(splitArea1, walls, doors);
+        SplitAreaRecursive(splitArea2, walls, doors);
     }
 }
